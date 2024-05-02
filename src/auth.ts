0b432106e20486b6f474aba2a9f75368a98decec
minhng-178 <anhminh1782002@gmail.com>
2024-05-02 14:58:11 +0700

    initial app

---
+++ src/auth.ts
@@ -0,0 +1,29 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import prisma from "./lib/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: "/logo.png",
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session({ session, user }) {
+      //? assign user role if need
+      // session.user.role = user.role;
+      return session;
+    },
+  },
+  providers: [
+    Google,
+    GitHub,
+    //? Resend for mail
+
+    // Resend({
+    //   from: "no-reply@tutorial.codinginflow.com",
+    // }),
+  ],
+});
---
+++ src/components/github-auth-button.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { Button } from "./ui/button";
+import { Icons } from "./icons";
+
+export default function GoogleSignInButton() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
+  return (
+    <>
+      <Button
+        className="w-full"
+        variant="outline"
+        type="button"
+        onClick={async () => {
+          await signIn("google", { callbackUrl: callbackUrl ?? "/dashboard" });
+        }}
+      >
+        <Icons.google className="mr-2 h-4 w-4" />
+        Continue with Google
+      </Button>
+
+      <Button
+        className="w-full"
+        variant="outline"
+        type="button"
+        onClick={() =>
+          signIn("github", { callbackUrl: callbackUrl ?? "/dashboard" })
+        }
+      >
+        <Icons.gitHub className="mr-2 h-4 w-4" />
+        Continue with Github
+      </Button>
+    </>
+  );
+}
