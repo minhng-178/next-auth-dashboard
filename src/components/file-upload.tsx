2280b614bde728053a4323c72a34a174febe2def
minhng-178 <anhminh1782002@gmail.com>
2024-05-03 09:00:00 +0700

    fix: type

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -3,15 +3,15 @@ import { OurFileRouter } from "@/app/api/uploadthing/core";
 import { UploadDropzone } from "@uploadthing/react";
 import { Trash } from "lucide-react";
 import Image from "next/image";
-import { UploadFileResponse } from "uploadthing/client";
+
 import { IMG_MAX_LIMIT } from "./forms/product-form";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
 
 interface ImageUploadProps {
   onChange?: any;
-  onRemove: (value: UploadFileResponse[]) => void;
-  value: UploadFileResponse[];
+  onRemove: (value: any) => void;
+  value: any;
 }
 
 export default function FileUpload({
@@ -25,7 +25,7 @@ export default function FileUpload({
     let filteredFiles = files.filter((item) => item.key !== key);
     onRemove(filteredFiles);
   };
-  const onUpdateFile = (newFiles: UploadFileResponse[]) => {
+  const onUpdateFile = (newFiles: any) => {
     onChange([...value, ...newFiles]);
   };
   return (
